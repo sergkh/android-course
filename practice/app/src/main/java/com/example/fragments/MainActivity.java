187aa4ff81ed5535120c3dc82a7acfe954428336
Sergey Khruschak <sergey.khruschak@fidesmo.com>
2025-04-12 22:34:32 +0300

    updated libs

--- practice/app/src/main/java/com/example/fragments/MainActivity.java
+++ practice/app/src/main/java/com/example/fragments/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
